d0a1919bc1da51835ef56280aefe94d8cf7052c2
Oliveira04 <augustocoimbra01@hotmail.com>
2023-09-06 13:41:27 -0300

    Agora fiz o embaralhamento das cartas, ou pelo menos tentei

--- source.cpp
+++ source.cpp
@@ -6,17 +6,20 @@ using namespace std;
 
 int main () {
         srand(time(0));
-    int i;
+    int index;
     Carta carta[52]; // vetor de 52 carta
     string naipes[] = {"P", "O", "C", "E"};
     //O for ta preenchendo o vetor com as 52 cartas
     for(int valor = 1; valor <= 13; valor++){
         for(int naipe = 0; naipe < 4; naipe++){
-            carta[i] = Carta(valor, naipes[naipe]);
-            i++;
+            carta[index] = Carta(valor, naipes[naipe]);
+            index++;
         }
     }
-
+    //Tentei fazer um embaralhamento das cartas
+    for(int i = 51; i > 0; i--){
+        swap(carta[i], carta[rand()%(i+1)]);
+    }
     
     Carta c1(10, "C");
     Carta c2(1, "P");
@@ -42,8 +45,8 @@ int main () {
 // O que eu acho que seriam os próximos passos: 
 // - Criar uma função que lista as linhas de uma forma mais "intuitiva", que nem o exemplo que o
 // professor deu na sala de aula. (Eu vou fazer isso, não se preocupem)
-// - Agente devia criar um vetor com 52 espaços no começo, e preencher eles com as cartas. 
-// Esse preenchimento devia ser aleatório.
+// - Agente devia criar um vetor com 52 espaços no começo, e preencher eles com as cartas. (ja estou fazendo isso - Augusto)
+// Esse preenchimento devia ser aleatório.(ja estou fazendo isso - Augusto)
 // - As freecells, um objetinho básico pra guardar uma única carta.
 // ----------------------------
 // Feito isso, falta começar a implementar a lógica de mover as cartas, pegar input do jogador, etc.
